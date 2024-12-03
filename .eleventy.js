475232355f656b59364460422e6ea7fcdc3b0a98
Jürgen Ratzenböck <juergen.ratzenboeck@gmail.com>
2024-12-03 14:13:39 +0100

    Add prettier and eslint

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,12 @@
 module.exports = function (eleventyConfig) {
   // Copy CSS and JavaScript files to the output folder
-  eleventyConfig.addPassthroughCopy("src/styles");
-  eleventyConfig.addPassthroughCopy("src/scripts");
+  eleventyConfig.addPassthroughCopy('src/styles');
+  eleventyConfig.addPassthroughCopy('src/scripts');
 
   return {
     dir: {
-      input: "src",
-      output: "_site",
+      input: 'src',
+      output: '_site',
     },
   };
 };
